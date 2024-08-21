d3ce6b775f9c0e65b36ea81cb9b1f980b848233e
Kasatria <nurizzatihh@gmail.com>
2024-08-21 18:18:17 +0800

    Fix CustomButton

--- app/contact.tsx
+++ app/contact.tsx
@@ -1,12 +1,30 @@
-import { View, Text } from 'react-native'
+import { View, Text, Pressable } from 'react-native'
 import React from 'react'
+import AppGradient from '@/components/AppGradient'
+import { StatusBar } from 'expo-status-bar'
+import { FlatList } from 'react-native-gesture-handler'
 
-const contact = () => {
+const Contact = () => {
   return (
     <View className='flex-1 justify-center items-center'>
-      <Text>Contact</Text>
+      <AppGradient colors={["#161b2e","#0a4d4a", "#766e67"]}>
+          <View className="mb-6">
+              <Text className="text-gray-200 mb-3 font-bold text-4xl text-left">
+                    Contact
+                </Text>
+                <Text className="text-indigo-100 text-xl font-medium">
+                    Start your app.
+                </Text>
+          </View>
+          <View>
+            <Pressable
+                onPress={() => console.log('press')}
+                className="h-48 my-3 font-bold"/>
+          </View>
+      </AppGradient>
+      <StatusBar style='light'/>
     </View>
   )
 }
 
-export default contact;
+export default Contact;
--- app/index.tsx
+++ app/index.tsx
@@ -3,9 +3,10 @@ import React from 'react'
 import { LinearGradient } from 'expo-linear-gradient';
 import { StatusBar } from 'expo-status-bar';
 import CustomButton from '@/components/CustomButton';
-import bgImage from '@/assets/images/thumb_600.png';
+import bgImage from '@/assets/images/jb_bg.jpg';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useRouter } from 'expo-router';
+import AppGradient from '@/components/AppGradient';
 
 const App = () => {
   const router = useRouter();
@@ -17,32 +18,27 @@ const App = () => {
       className='flex-1'
       
       >
-        <LinearGradient 
-        className='flex-1' 
-        colors={["rgb(0,00,0.4", "rgba(0,0,0,0.8)"]}
-        >
-          <SafeAreaView className="flex-1 mx-5 my-12 justify-between">
-            <View>
-              <Text className="text-center text-black font-bold text-4xl">
-                App Demo Test
-              </Text>
-              <Text className="text-center text-black">
-                  Testing for React
-              </Text>
-            </View>
 
-            <View>
-              <CustomButton 
-                onPress={() => router.push("/contact")} 
-                title='Get Started'
-                />
-            </View>
-
-            <StatusBar style="light"/>
-            </SafeAreaView>
-        </LinearGradient>
-      
-      </ImageBackground>
+        <AppGradient colors={["rgba(0,0,0,0.4","rgba(0,0,0,0.8"]}>
+            <SafeAreaView className="flex-1 px-1 justify-between">
+              <View>
+                <Text className="text-center text-white font-bold text-4xl">
+                  App Demo Test
+                </Text>
+                <Text className="text-center text-white">
+                    Testing for React
+                </Text>
+              </View>
+              <View>
+                <CustomButton
+                  onPress={() => router.push("/contact")}
+                  title='Get Started'
+                  />
+              </View>
+              <StatusBar style="light"/>
+              </SafeAreaView>
+        </AppGradient>
+       </ImageBackground>
       </View>
   )
 }
